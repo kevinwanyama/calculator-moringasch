233a51158b6fdd53efdebc6bc727c3d5b199f215
kevinwanyama <kvnwanyama@gmail.com>
2022-04-26 07:35:14 +0300

    simple calculater using html,js and css

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var add = function(number1, number2){
+    return number1 + number2;
+    };
+    var number1 = parseInt(prompt("Enter a number:"));
+    var number2 = parseInt(prompt("Enter another number:"));
+    alert(add(number1, number2));
+    
+    var substract = function(number1, number2){
+    return number1 - number2};
+    alert(subtract(10,5));
+    
