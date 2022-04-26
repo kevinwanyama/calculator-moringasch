fcaf2aadab7e247fcdecd1adf77a162fa65ed601
kevinwanyama <kvnwanyama@gmail.com>
2022-04-26 16:01:06 +0300

    scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+});
 var add = function(number1, number2){
     return number1 + number2;
-    };
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    alert(add(number1, number2));
-    
-    var substract = function(number1, number2){
-    return number1 - number2};
-    alert(subtract(10,5));
-    
+};
+var substract = function(number1, number2){
+    return number1 - number2;
+};
+var multiply = function(number1, number2){
+    return number1 * number2;
+};
+var divide = function(number1, number2){
+    return number1 / number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var results = divide(number1, number2);
+alert(results);
