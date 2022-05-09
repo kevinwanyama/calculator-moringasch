3613df1d463b7c6e812e5204e8732fa3568a91f9
kevinwanyama <kvnwanyama@gmail.com>
2022-05-09 15:21:50 +0300

    moringa

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
-jQuery('h1').click(function(){
-    alert("This is a heading tag")
-});
+// jQuery('h1').click(function(){
+//     alert("This is a heading tag")
+// });
 var add = function(number1, number2){
     return number1 + number2;
 };
@@ -13,7 +13,15 @@ var multiply = function(number1, number2){
 var divide = function(number1, number2){
     return number1 / number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var results = divide(number1, number2);
-alert(results);
+
+$(document).ready (function(){
+$("form#add").submit(function(event) {
+event.preventDefault();
+var number1 = parseInt($("#add1").val());
+var number2 = parseInt($("#add2").val());
+// var results = divide(number1, number2);
+var result = (add(number1, number2));
+$("#output").text(result);
+});
+});
+
